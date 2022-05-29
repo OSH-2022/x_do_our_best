2d85f46854cb60ffbdaa31fc04a3dfc4c25a4812
luzimu <luzimu@mail.ustc.edu.cn>
2022-05-29 15:42:10 +0800

    add research for mutlibot

---
+++ reference/multiple_cars.md
@@ -0,0 +1,7 @@
+ [(8条消息) ROS，多车同时导航，多机协同_zlb_zlb的博客-CSDN博客_多车协同](https://blog.csdn.net/zlb_zlb/article/details/106973613) 
+
+ [(8条消息) ros怎么跑小车_ros模拟多小车控制运动_JOIN于宙的博客-CSDN博客](https://blog.csdn.net/weixin_33610842/article/details/113018422) 
+
+ [(8条消息) 基于ROS节点多机协同(无人机/无人车)分析_阿木实验室的博客-CSDN博客_无人机与无人车协同代码](https://blog.csdn.net/msq19895070/article/details/106719404) 
+
+ [airuchen/multi_turtlebot3: ROS packages for Turtlebot3 (github.com)](https://github.com/airuchen/multi_turtlebot3) 
---
+++ src/ros_package/ros_cmd_vel_pub.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+ 
+ import rospy
+ from geometry_msgs.msg import Twist
+ from pynput.keyboard import Key, Listener
+ 
+ vel = Twist()
+ vel.linear.x = 0
+ 
+def on_press(key):
+
+    try:
+        if(key.char == 'w'):
+            print("Forward")
+            vel.linear.x = 0.8
+           vel.angular.z = 0
+
+        if(key.char == 's'):
+            print("Backward")
+            vel.linear.x = -0.8
+            vel.angular.z = 0
+
+        if(key.char == 'a'):
+            print("Counter Clockwise")
+            vel.linear.x = 0
+            vel.angular.z = -0.8
+
+        if(key.char == 'd'):
+            print("Clockwise")
+            vel.linear.x = 0
+            vel.angular.z = 0.8
+
+        return False
+
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+        return False
+
+def on_release(key):
+    vel.linear.x = 0
+    vel.angular.z = 0
+
+    return False
+
+# Init Node
+rospy.init_node('my_cmd_vel_publisher')
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+# Set rate
+rate = rospy.Rate(10)
+
+listener = Listener(on_release=on_release, on_press = on_press)
+
+while not rospy.is_shutdown():
+    print(vel.linear.x)
+    pub.publish(vel)
+    vel.linear.x = 0
+    vel.angular.z = 0
+    rate.sleep()
+
+    if not listener.running:
+        listener = Listener(on_release=on_release, on_press = on_press)
+        listener.start()
+ 

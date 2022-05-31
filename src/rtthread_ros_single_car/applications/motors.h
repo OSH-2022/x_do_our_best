7447383e92b00821a94e4031d89298ea5a5a7c5d
luzimu <luzimu@mail.ustc.edu.cn>
2022-05-31 18:19:03 +0800

    add single car project, can control single car with ros_rtthread

---
+++ src/rtthread_ros_single_car/applications/motors.h
@@ -0,0 +1,20 @@
+#include <rtthread.h>
+
+ class MotorControl {
+   public:
+     //Var
+     rt_uint32_t  maxSpd;
+     float moveFactor;
+     float turnFactor;
+
+    MotorControl(int fl_engine, int fl_turn);
+    void initMotors();
+    void rotateBot(float spd, float bias);
+    void moveBot(float spd, float bias);
+    void stopMotors();
+  private:
+    struct rt_device_pwm *pwm_dev;
+    //The pins
+    int fl_engine;
+    int fl_turn;
+};

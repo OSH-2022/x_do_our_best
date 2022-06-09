c89817ba264fda11c0af2c5de04e5f71afab71f0
Vedrige <tangxing@mail.ustc.edu.cn>
2022-06-09 20:33:39 +0800

    ros control v2

--- src/ros_multi_car_package/ros_cmd_vel_pub.py
+++ src/ros_multi_car_package/ros_cmd_vel_pub.py
@@ -2,46 +2,7 @@
 
 import rospy
 from Location_Array import Location_Array
-#from geometry_msgs.msg import Twist
-from pynput.keyboard import Key, Listener
-
-vel = Twist()
-vel.linear.x = 0
-
-def on_press(key):
-
-    try:
-        if(key.char == 'w'):
-            print("Forward")
-            vel.linear.x = 0.8
-            vel.angular.z = 0
-
-        if(key.char == 's'):
-            print("Backward")
-            vel.linear.x = -0.8
-            vel.angular.z = 0
-
-        if(key.char == 'a'):
-            print("Counter Clockwise")
-            vel.linear.x = 0
-            vel.angular.z = -0.8
-
-        if(key.char == 'd'):
-            print("Clockwise")
-            vel.linear.x = 0
-            vel.angular.z = 0.8
-
-        return False
-
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-        return False
-
-def on_release(key):
-    vel.linear.x = 0
-    vel.angular.z = 0
-
-    return False
+from std_msgs.msg import int
 
 def callback():
 pub = rospy.Publisher('map', Location_Array, queue_size=10) #把地图发给小车B
@@ -54,8 +15,8 @@ while not rospy.is_shutdowm();
      
 def run():
 # Init Node
-rospy.init_node('my_cmd_vel_publisher')
-rospy.Subscriber('start', , queue_size=10)# 接收start信号
+rospy.init_node('listen&pub')
+rospy.Subscriber('start', int ,callback,queue_size=10)# 接收start信号 回调函数callback
 rospy.spin()
 
 if __name__=='__main__'
